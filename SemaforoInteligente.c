1d24c9b6aabf41521113aaef261321e31a393a8f
wilwtiher <wiltonjrtablet@gmail.com>
2025-05-02 16:51:39 -0300

    importacao projeto blinkConta

--- SemaforoInteligente.c
+++ SemaforoInteligente.c
@@ -1,14 +1,110 @@
-#include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/i2c.h"
+#include "lib/ssd1306.h"
+#include "lib/font.h"
+#include "FreeRTOS.h"
+#include "FreeRTOSConfig.h"
+#include "task.h"
+#include <stdio.h>
 
+#define I2C_PORT i2c1
+#define I2C_SDA 14
+#define I2C_SCL 15
+#define endereco 0x3C
 
+#define led1 11
+#define led2 12
+
+void vBlinkLed1Task()
+{
+    gpio_init(led1);
+    gpio_set_dir(led1, GPIO_OUT);
+    while (true)
+    {
+        gpio_put(led1, true);
+        vTaskDelay(pdMS_TO_TICKS(250));
+        gpio_put(led1, false);
+        vTaskDelay(pdMS_TO_TICKS(1223));
+    }
+}
+
+void vBlinkLed2Task()
+{
+    gpio_init(led2);
+    gpio_set_dir(led2, GPIO_OUT);
+    while (true)
+    {
+        gpio_put(led2, true);
+        vTaskDelay(pdMS_TO_TICKS(500));
+        gpio_put(led2, false);
+        vTaskDelay(pdMS_TO_TICKS(2224));
+    }
+}
+
+void vDisplay3Task()
+{
+    // I2C Initialisation. Using it at 400Khz.
+    i2c_init(I2C_PORT, 400 * 1000);
+
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);                    // Set the GPIO pin function to I2C
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);                    // Set the GPIO pin function to I2C
+    gpio_pull_up(I2C_SDA);                                        // Pull up the data line
+    gpio_pull_up(I2C_SCL);                                        // Pull up the clock line
+    ssd1306_t ssd;                                                // Inicializa a estrutura do display
+    ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT); // Inicializa o display
+    ssd1306_config(&ssd);                                         // Configura o display
+    ssd1306_send_data(&ssd);                                      // Envia os dados para o display
+    // Limpa o display. O display inicia com todos os pixels apagados.
+    ssd1306_fill(&ssd, false);
+    ssd1306_send_data(&ssd);
+
+    char str_y[5]; // Buffer para armazenar a string
+    int contador = 0;
+    bool cor = true;
+    while (true)
+    {
+        sprintf(str_y, "%d", contador); // Converte em string
+        contador++;                     // Incrementa o contador
+        ssd1306_fill(&ssd, !cor);                          // Limpa o display
+        ssd1306_rect(&ssd, 3, 3, 122, 60, cor, !cor);      // Desenha um retângulo
+        ssd1306_line(&ssd, 3, 25, 123, 25, cor);           // Desenha uma linha
+        ssd1306_line(&ssd, 3, 37, 123, 37, cor);           // Desenha uma linha
+        ssd1306_draw_string(&ssd, "CEPEDI   TIC37", 8, 6); // Desenha uma string
+        ssd1306_draw_string(&ssd, "EMBARCATECH", 20, 16);  // Desenha uma string
+        ssd1306_draw_string(&ssd, "  FreeRTOS", 10, 28); // Desenha uma string
+        ssd1306_draw_string(&ssd, "Contador  LEDs", 10, 41);    // Desenha uma string
+        ssd1306_draw_string(&ssd, str_y, 40, 52);          // Desenha uma string
+        ssd1306_send_data(&ssd);                           // Atualiza o display
+        sleep_ms(735);
+    }
+}
+
+// Trecho para modo BOOTSEL com botão B
+#include "pico/bootrom.h"
+#define botaoB 6
+void gpio_irq_handler(uint gpio, uint32_t events)
+{
+    reset_usb_boot(0, 0);
+}
 
 int main()
 {
+    // Para ser utilizado o modo BOOTSEL com botão B
+    gpio_init(botaoB);
+    gpio_set_dir(botaoB, GPIO_IN);
+    gpio_pull_up(botaoB);
+    gpio_set_irq_enabled_with_callback(botaoB, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+    // Fim do trecho para modo BOOTSEL com botão B
+
     stdio_init_all();
 
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
-    }
+    xTaskCreate(vBlinkLed1Task, "Blink Task Led1", configMINIMAL_STACK_SIZE,
+         NULL, tskIDLE_PRIORITY, NULL);
+    xTaskCreate(vBlinkLed2Task, "Blink Task Led2", configMINIMAL_STACK_SIZE, 
+        NULL, tskIDLE_PRIORITY, NULL);
+    xTaskCreate(vDisplay3Task, "Cont Task Disp3", configMINIMAL_STACK_SIZE, 
+        NULL, tskIDLE_PRIORITY, NULL);
+    vTaskStartScheduler();
+    panic_unsupported();
 }
